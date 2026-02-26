648908a776c8d382f134bdcccab6b7b1e5fc8c07
Vladimir Mitroi <mitroialexandru23@stud.ase.ro>
2026-02-26 17:22:48 +0200

    Seminar 02

---
+++ Seminar02/src/ro/ase/cts/clase/loaders/AplicantiLoader.java
@@ -0,0 +1,26 @@
+package ro.ase.cts.clase.loaders;
+
+import ro.ase.cts.clase.Aplicant;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public abstract class AplicantiLoader {
+    public abstract List<Aplicant> loadAplicants(String file) throws FileNotFoundException;
+    public void loadAplicant(Scanner input, Aplicant aplicant){
+        String nume = input.next();
+        String prenume = input.next();
+        int varsta = input.nextInt();
+        int punctaj = input.nextInt();
+        int nr = input.nextInt();
+        String[] vect = new String[5];
+        for (int i = 0; i < nr; i++)
+            vect[i] = input.next();
+        aplicant.setNume(nume);
+        aplicant.setPrenume(prenume);
+        aplicant.setVarsta(varsta);
+        aplicant.setPunctaj(punctaj);
+        aplicant.setNr_proiecte(nr,vect);
+    }
+}
---
+++ Seminar02/src/ro/ase/cts/main/Program.java
@@ -0,0 +1,25 @@
+package ro.ase.cts.main;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.loaders.AngajatiLoader;
+import ro.ase.cts.clase.loaders.AplicantiLoader;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAngajati;
+		try {
+			AplicantiLoader loader=new AngajatiLoader();
+			listaAngajati = loader.loadAplicants("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
