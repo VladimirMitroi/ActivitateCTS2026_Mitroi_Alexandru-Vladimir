8c01ac5da268e699fc87289e01e098e2667f36be
Vladimir Mitroi <mitroialexandru23@stud.ase.ro>
2026-03-17 19:04:05 +0200

    Seminar 05

--- Builder/src/ro/ase/cts/main/Program.java
+++ Builder/src/ro/ase/cts/main/Program.java
@@ -3,6 +3,9 @@ package ro.ase.cts.main;
 import ro.ase.cts.builder01.AbstractBuilder;
 import ro.ase.cts.builder01.Internare;
 import ro.ase.cts.builder01.InternareBuilder;
+import ro.ase.cts.builder02.AbstractBuilder02;
+import ro.ase.cts.builder02.Internare02;
+import ro.ase.cts.builder02.InternareBuilder02;
 
 public class Program {
     public static void main(String[] args){
@@ -20,5 +23,15 @@ public class Program {
         Internare internare4=builder.setNumePacient("Gigel").setHalatInterior(true).setPapuciCamera(true).build();
         System.out.println(internare3);
         System.out.println(internare4);
+
+        AbstractBuilder02 builder02=new InternareBuilder02();
+        builder02.setMicDejunInclus(true);
+        Internare02 internare5=builder02.build("Alex");
+        Internare02 internare6=builder02.build("Mihai");
+        System.out.println(internare5);
+        System.out.println(internare6);
+
+        internare5.setHalatInterior(true);
+        System.out.println(internare5);
     }
 }
